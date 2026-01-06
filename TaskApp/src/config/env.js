eb7b3627094212def660e4006ca2e353d3e79bda
PRAKASH-SK <prakashpv.cs24@bitsathy.ac.in>
2026-01-06 14:09:16 +0530

    feat: Update API_URL to correct endpoint

--- TaskApp/src/config/env.js
+++ TaskApp/src/config/env.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://10.150.255.247:3000/api';
+export const API_URL = 'http://10.150.255.195:3000/api';
 export const GOOGLE_WEB_CLIENT_ID = '720697957982-i60kch70ehbp3ht0kp3ecb7ulgeaissb.apps.googleusercontent.com';
 
