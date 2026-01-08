b297c77738804163cfa77e339d0fc1b0c91ed248
PRANESH KARTHI M S <107921577+mspk5196@users.noreply.github.com>
2026-01-08 09:56:50 +0530

    feat: Update API URL and add google-services.json to .gitignore

--- TaskApp/src/config/env.js
+++ TaskApp/src/config/env.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://10.150.255.247:3000/api';
+export const API_URL = 'http://10.150.252.99:3000/api';
 export const GOOGLE_WEB_CLIENT_ID = '1036739290914-vhfitov4kpvlemh8fjd7jli57gnsn0vl.apps.googleusercontent.com';
 
